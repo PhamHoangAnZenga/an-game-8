fccae774c6902a4e462828c257efb329ba67219a
KNSBA <phamhoangan@zenga.com.vn>
2026-08-19 09:17:20 +0700

    remove DOTween

--- Data project/Assets/Project/Screens/LoadScreen/LoadScreenController.cs
+++ Data project/Assets/Project/Screens/LoadScreen/LoadScreenController.cs
@@ -1,23 +1,20 @@
 using UnityEngine;
 using SS.UI;
 using TMPro;
-using System;
-using UnityEngine.UI;
 
 public class LoadScreenController : MonoBehaviour, IKeyBack
 {
     public const string NAME = "LoadScreen";
     [SerializeField] TextMeshProUGUI _loadingText;
     int _timer;
-    
+
     public void OnKeyBack()
     {
         Core.Close();
     }
 
-    void Update()
+    void Start()
     {
-        if
-        _loadingText = Text[i];
+        _loadingText.DOText();
     }
 }
